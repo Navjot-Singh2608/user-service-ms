eb5fcfe8165681a84e3ab9b21a2a14dc37344c2a
navjotsingh2608 <navjot@argenelec.com>
2024-06-06 15:46:18 -0400

    update

---
+++ src/main/java/com/fooddelivery/userinfo/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.fooddelivery.userinfo.mapper;
+
+import com.fooddelivery.userinfo.dto.UserDTO;
+import com.fooddelivery.userinfo.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User mapUserDTOToUser(UserDTO userDTO);
+    UserDTO mapUserToUserDTO(User user);
+
+}
---
+++ src/main/java/com/fooddelivery/userinfo/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.fooddelivery.userinfo.repo;
+
+import com.fooddelivery.userinfo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+}
